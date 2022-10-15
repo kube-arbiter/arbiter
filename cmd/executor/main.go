717c352d857e0db3462916dbed617c6e2b461de4
lizhen <lizhen@outlook.jp>
2022-10-15 14:58:23 +0800

    context logger with klog, flag parsing plugin

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -20,16 +20,15 @@ import (
 	"flag"
 	"os"
 
+	arbiterv1alpha1 "github.com/kube-arbiter/arbiter/pkg/apis/v1alpha1"
+	"github.com/kube-arbiter/arbiter/pkg/common"
+	"github.com/kube-arbiter/arbiter/pkg/executor"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	arbiterv1alpha1 "github.com/kube-arbiter/arbiter/pkg/apis/v1alpha1"
-	"github.com/kube-arbiter/arbiter/pkg/executor"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
@@ -64,23 +63,24 @@ func main() {
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.StringVar(&sockAddr, "endpoint", "/var/run/resourcetagger.sock", "sock file path")
 
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
+	common.DoRegisterFlags(flag.CommandLine)
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	logger := common.NewLogr()
+	ctrl.SetLogger(logger)
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
-		Namespace:              os.Getenv("NAMESPACE"),
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "2d27424e.k8s.com.cn",
-	})
+	mgr, err := ctrl.NewManager(
+		common.CustomizeRestConfig(ctrl.GetConfigOrDie()),
+		*common.CustomizeControllerOptions(&ctrl.Options{
+			Scheme:                 scheme,
+			MetricsBindAddress:     metricsAddr,
+			Port:                   9443,
+			Namespace:              os.Getenv("NAMESPACE"),
+			HealthProbeBindAddress: probeAddr,
+			LeaderElection:         enableLeaderElection,
+			LeaderElectionID:       "2d27424e.k8s.com.cn",
+			Logger:                 logger,
+		}))
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
